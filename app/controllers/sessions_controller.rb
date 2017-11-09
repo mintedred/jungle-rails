add9cb73bed5966da00981f3cf4814945f939fbd
Corina Wong <hi@corinawong.com>
2017-11-09 23:55:00 +0000

    Fix form design for sign-up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-    # If user's login doesn't work, send them back to the login form.
+    # If user's login doesn't work, send them back to the login form.\
       redirect_to '/login'
     end
   end
